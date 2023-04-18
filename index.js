91bd99999573e3ddb4e492219aab76165436261c
Subodh Sharma <sharma2000subodh@gmail.com>
2023-04-18 22:44:00 +0530

    remove cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.json({limit:"30mb",extended:true}));
 app.use(express.urlencoded({limit:"30mb",extended: true}));
-app.use(cors());
+// app.use(cors());
 
 
 // app.get("/",(req,res)=>{
@@ -61,6 +61,7 @@ mongoose.connect(process.env.DB_URL,{
         res.setHeader("Access-Control-Allow-Origin", "*");
      });
     const io = new Server(server,{
+        pingTimeout: 60000,
         cors: {
             origin: "*"
         }
